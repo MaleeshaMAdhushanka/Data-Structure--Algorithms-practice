b1f7451197ad8c125fae2c18a2c22ba36a4a7fca
MaleeshaMAdhushanka <zeejmaleeshamadushanka@gmail.com>
2024-07-31 11:06:28 +0530

    Comments selection sort

--- src/main/java/lk/ijse/SelectionSort.java
+++ src/main/java/lk/ijse/SelectionSort.java
@@ -4,25 +4,57 @@ import java.util.Arrays;
 
 public class SelectionSort {
     public static void main(String[] args) {
-        int [] nums = {-5, 7, 2, -8, 4, 4, 0, 3, 50, 45, 25, -20};
-         //The original unsorted array is printed using Arrays.to String() method
+                     //0, 1, 2,  3, 4, 5, 6, 7, 8,  9,  10,  11
+       int [] nums = {-5, 7, 2, -8, 4, 4, 0, 3, 50, 45, 25, -20};
+
+
+       //-5 ewa check karala ita passe k = 1 wenawa
+
 
         System.out.println(Arrays.toString(nums));
 
+
         for (int k = 0; k < nums.length; k++) {
-            int minIndex = k;
-            int min = nums[minIndex];
+            //k = 0;
+            //k = 1
+            int minIndex = k; //minIndex = 0 , den 8 element eka nisa methanata 3 index eka dagannwa
+            //11
+            int min = nums[minIndex];  //min value eka dagannwa = -5, index ekai value ekai ganna,
+            //den methana min -8
+            //min -20
 
-            for (int i = (k +1); i < nums.length; i++) {
+            //1 thiyena value eka 7 wenawa
 
+            //dewani forloop ckeck kranwa -5 ta wada adu agayan thiyenawada kiyala
+            for (int i = (k+1); i < nums.length; i++) {
+                /*values deken  kuda eka mokadd kiyala check karanwa
+                -5 , 7
+                -5, 2
+                -5, -8
+                dem -5 ta wada adu agayak hambunu nisa
+                -8, 4
+                -8,20
+
+                 */
                 if (min > nums[i]) {
                     min = nums[i];
                     minIndex = i;
+
                 }
+
             }
-            nums[minIndex] = nums[k];
-            nums[k] = min;
+            //array eke miniumam index eka mokadd kiyala balanwa 11 kotuwata values deka maru karabwa
+
+          nums[minIndex] = nums[k];
+//                          //0, 1, 2,  3, 4, 5, 6, 7, 8,  9,  10,  11
+//            int [] nums = {-5, 7, 2, -8, 4, 4, 0, 3, 50, 45, 25, -5};
+
+            nums[k] =  min;
+            // int [] nums = {-20, 7, 2, -8, 4, 4, 0, 3, 50, 45, 25, -5};
+
+
         }
         System.out.println(Arrays.toString(nums));
     }
+    //select karala eka mathaka thiyagena ne pitipassata danne
 }

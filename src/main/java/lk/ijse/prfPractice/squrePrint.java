87d9c1e386e072c876b9751336203322354dd386
MaleeshaMAdhushanka <zeejmaleeshamadushanka@gmail.com>
2024-08-05 15:45:25 +0530

    sqrePrint

---
+++ src/main/java/lk/ijse/prfPractice/squrePrint.java
@@ -0,0 +1,12 @@
+package lk.ijse.prfPractice;
+
+public class squrePrint {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
